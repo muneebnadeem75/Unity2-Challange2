370a3123b0bf1aa2dc6445c40557f68b4cd63ea4
muneebnadeem75 <b00158381@mytudublin.ie>
2023-10-16 19:48:12 +0100

    First commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float fireRate = 1;
+    private float nextFire = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, if enough time has elapsed since last fire, send dog
+        if (Input.GetKeyDown(KeyCode.Space)&& Time.time > nextFire)
+        {
+        
+            nextFire = Time.time + fireRate;// reset nextFire to current time + fireRate
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            }
+        }
+    }
+
